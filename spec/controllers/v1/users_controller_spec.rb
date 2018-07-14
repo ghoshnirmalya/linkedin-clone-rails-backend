64210023229176cdac41339f8481c88eba24d167
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-14 15:02:05 +0530

    feat(versioning): Add versioning
    - Add v1 versioning to all routes
    - Add /v1 to all routes

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -23,7 +23,7 @@ require 'rails_helper'
 # removed from Rails core in Rails 5, but can be added back in via the
 # `rails-controller-testing` gem.
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe V1::UsersController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
