443dc7ab729fb315fad4bbbd57959e8697a75cc3
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-21 01:44:23 +0530

    chore(seed): Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+100.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::String.random(6),
+  )
+end
+
+# Companies
+100.times do
+  Company.create(
+    name: Faker::Company.name,
+  )
+end
+
+# Jobs
+100.times do
+  Job.create(
+    title: Faker::Job.title,
+    description: Faker::Job.field,
+    company_id: Faker::Number.number(1),
+  )
+end
