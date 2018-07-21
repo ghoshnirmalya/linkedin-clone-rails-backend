4f7bedd71aba8513c301920ec441e0bbfe6a882b
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-22 02:53:16 +0530

    test(models): Add specs for Conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,8 +1,9 @@
 class Conversation < ApplicationRecord
+  validates_presence_of :sender_id, :recipient_id
+  validates_uniqueness_of :sender_id, :scope => :recipient_id
+
   belongs_to :sender, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
 
   has_many :messages, dependent: :destroy
-
-  validates_uniqueness_of :sender_id, :scope => :recipient_id
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Conversation do
+    it { is_expected.to validate_presence_of(:sender_id) }
+    it { is_expected.to validate_presence_of(:recipient_id) }
+  end
 end
