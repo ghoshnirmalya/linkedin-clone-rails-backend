cc4fa13c0e611380063844fcee434c9e73a63109
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-22 02:54:15 +0530

    test(models): Update specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe User do
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
   end
 end
