8e7eccdad3b7f3650d3750eccbc7bc1c6fc1ff31
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-23 01:04:56 +0530

    test(controller): Update specs for Authentication

--- spec/controllers/v1/authentication_controller_spec.rb
+++ spec/controllers/v1/authentication_controller_spec.rb
@@ -22,6 +22,8 @@ RSpec.describe V1::AuthenticationController, type: :controller do
 
   describe "#sign_up" do
     context "with valid params" do
+      let(:mail) { UserMailer.welcome_email(User.last.id) }
+
       it "creates a new User" do
         post :sign_up, params: {data: valid_attributes}
 
@@ -34,6 +36,15 @@ RSpec.describe V1::AuthenticationController, type: :controller do
         expect(response).to be_successful
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)["auth_token"]).to eq("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.MGDoh6fr29U1-DQo0o3myLlj3i2-fQclV9mQ1tDKIfw")
+      end
+
+      it "sends a mail" do
+        post :sign_up, params: {data: valid_attributes}
+
+        expect(ActionMailer::Base.deliveries.count).to eq(3)
+        expect(mail.subject).to eq("Getting started with LinkedIn Rails")
+        expect(mail.from).to eq([ENV.fetch("SENDGRID_DOMAIN")])
+        expect(mail.to).to eq([User.last.email])
       end
     end
 
@@ -62,7 +73,7 @@ RSpec.describe V1::AuthenticationController, type: :controller do
 
         expect(response).to be_successful
         expect(response).to have_http_status(:ok)
-        expect(JSON.parse(response.body)["auth_token"]).to eq("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozfQ.IVXk2dJkHk4Vcc9lK1eQeOEfWmftLnrc5zhY311K8Q8")
+        expect(JSON.parse(response.body)["auth_token"]).to eq("eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0fQ.cdT5O1Lcg28FebC8tTO9mWxRiyaycodAO9-VrLtMY7s")
       end
 
       it "doesn't create a new User" do
