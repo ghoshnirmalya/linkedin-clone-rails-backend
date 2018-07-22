40056987422c2d2ced62023007ee00f04e3e8084
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-07-23 02:29:25 +0530

    test(mailers): Update specs for UserMailer

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "notify" do
+  describe "welcome_email" do
     user = FactoryBot.build(:user)
     let(:mail) { UserMailer.welcome_email(user.id) }
 
@@ -9,6 +9,19 @@ RSpec.describe UserMailer, type: :mailer do
       user = FactoryBot.create(:user)
 
       expect(mail.subject).to eq("Getting started with LinkedIn Rails")
+      expect(mail.from).to eq([ENV.fetch("SENDGRID_DOMAIN")])
+      expect(mail.to).to eq([User.last.email])
+    end
+  end
+
+  describe "forgot_password_email" do
+    user = FactoryBot.build(:user)
+    let(:mail) { UserMailer.forgot_password_email(user.id, "some_random_password") }
+
+    it "renders the headers" do
+      user = FactoryBot.create(:user)
+
+      expect(mail.subject).to eq("Reset your password at LinkedIn Rails")
       expect(mail.from).to eq([ENV.fetch("SENDGRID_DOMAIN")])
       expect(mail.to).to eq([User.last.email])
     end
